582f88c7e799aed225f9fbba917bd20458eab965
Mohan <smohan0305@gmail.com>
2024-05-16 13:58:33 +0530

    removed the unused count variable

--- ReverseNoPattern/src/ReverseNoPattern.java
+++ ReverseNoPattern/src/ReverseNoPattern.java
@@ -5,9 +5,7 @@ public class ReverseNoPattern{
 		System.out.print("Enter the n numbers :");
 		int n=input.nextInt(); // input for n numbers
 		input.close();
-		int count=0; // initial count is 0
 		while(n>0) {
-			count++;
 			System.out.println(n+" ");
 			n=n/10;
 		}
